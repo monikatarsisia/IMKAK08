3cdd46e619895164804b3a5e0a6a2d9fc07fadd9
Monika Tarsisia Nangoi <monikatarsisia@hotmail.com>
2016-04-30 16:47:28 +0700

    Update infrared.ino

--- infrared.ino
+++ infrared.ino
@@ -1,6 +1,6 @@
  int pd=12;                      //Photodiode to digital pin 2
 
- int buzz=13;                   //piezo buzzer to digital pin 13  
+ int buzz=13;                   //LED to digital pin 13  
  int senRead=0;                 //Readings from sensor to analog pin 0  
  int limit = 850;                 //Threshold range of an obstacle  
  void setup()    
@@ -8,7 +8,7 @@
   pinMode(pd,OUTPUT);  
   pinMode(buzz,OUTPUT);  
   digitalWrite(pd,HIGH);       //supply 5 volts to photodiode  
-  digitalWrite(buzz,LOW);      //set the buzzer in off mode (initial condition)  
+  digitalWrite(buzz,LOW);      //set the LED in off mode (initial condition)  
   Serial.begin(9600);          //setting serial monitor at a default baund rate of 9600  
  }  
  void loop()  
@@ -17,12 +17,12 @@
   Serial.println(val);          // prints the values from the sensor in serial monitor  
   if(val <= limit)              //If obstacle is not in Threshold range  
   {  
-   digitalWrite(buzz,LOW);     // Buzzer will be in OFF state  
+   digitalWrite(buzz,LOW);     // LED will be in OFF state  
    delay(20);  
   }  
   else if(val > limit)          //If obstacle is nearer than the Threshold range
   {  
-   digitalWrite(buzz,HIGH);      //Buzzer will be in ON state
+   digitalWrite(buzz,HIGH);      // LED will be in ON state
    delay(20);  
   }  
  }  
